5cf6ae44bd6d895578d6b7fad98337e32198ccfa
Glitch (starter-cra-and-express) <none>
2023-10-16 09:46:17 +0000

    ⛺️🚗 Checkpoint ./src/SingleMovie.js:62151714/3084 ./src/App.js:62151714/4070 ./src/App.css:62151714/359

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import SingleMovie from './SingleMovie'; // Import the SingleMovieDetails component
+import { BrowserRouter as Router, Route, Routes,Link,useParams } from 'react-router-dom';
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -21,6 +20,7 @@ function App() {
 
     fetchData();
   }, []);
+  
 
   return (
     <Router>
@@ -39,9 +39,53 @@ function App() {
         </div>
       </div>
       <Routes>
-       <Route path="/movie/:id" component={SingleMovie} />
+        <Route path="/movie/:id" element={<SingleMovie movie={movies[0]} />} />
       </Routes>
     </Router>
+  );
+}
+function SingleMovie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    async function fetchMovieDetails() {
+      try {
+        const response = await fetch(`/api/movie/${id}`);
+        if (response.ok) {
+          const data = await response.json();
+          setMovie(data.data);
+        }
+      } catch (error) {
+        console.error('Error fetching movie data:', error);
+      }
+    }
+
+    fetchMovieDetails();
+  }, [id]);
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
+  // Format release date using browser's locale settings
+  const formattedReleaseDate = new Date(movie.release_date).toLocaleDateString();
+
+  // Calculate runtime in minutes
+  const runtimeInMinutes = `${Math.floor(movie.runtime / 60)} minutes`;
+
+  return (
+    <div className="single-movie">
+      <h1>{movie.title}</h1>
+      <p>Tagline: {movie.tagline}</p>
+      <p>Vote Average: {movie.vote_average}/10</p>
+      <p>Release Date: {formattedReleaseDate}</p>
+      <p>Runtime: {runtimeInMinutes}</p>
+      <p>Status: {movie.status}</p>
+      <p>Overview: {movie.overview}</p>
+      {/* Add more fields as needed */}
+      <Link to="/">Go Back</Link>
+    </div>
   );
 }
 
