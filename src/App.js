ecc167bdb76f2a37484c8aceb27bcd0ab4427761
Glitch (starter-cra-and-express) <none>
2023-10-16 08:36:15 +0000

    👩‍👩‍👦🎬 Checkpoint ./src/App.js:62151714/8029

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-function SingleMovie({ movie }) {
-  if (!movie) {
-    return <div>No movie selected</div>;
-  }
-
-  return (
-    <div className="single-movie">
-      <h1>{movie.title}</h1>
-      <p>Tagline: {movie.tagline}</p>
-      <p>Vote Average: {movie.vote_average}/10</p>
-      <p>Release Date: {new Date(movie.release_date).toLocaleDateString()}</p>
-      <p>Runtime: {movie.runtime} minutes</p>
-      <Link to="/">Go Back</Link>
-    </div>
-  );
-}
+import { BrowserRouter as Router, Route, Link, Outlet, useParams } from 'react-router-dom'; // Import useParams
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -54,10 +37,28 @@ function App() {
           ))}
         </div>
       </div>
-      <Routes>
-        <Route path="/movie/:id" element={<SingleMovie movie={movies[0]} />} />
-      </Routes>
+      <Outlet />
     </Router>
+  );
+}
+
+function SingleMovie() {
+  const { id } = useParams(); // Use useParams to get the movie ID from the URL
+  const movie = movies.find((m) => m.id === parseInt(id));
+
+  if (!movie) {
+    return <div>No movie selected</div>;
+  }
+
+  return (
+    <div className="single-movie">
+      <h1>{movie.title}</h1>
+      <p>Tagline: {movie.tagline}</p>
+      <p>Vote Average: {movie.vote_average}/10</p>
+      <p>Release Date: {new Date(movie.release_date).toLocaleDateString()}</p>
+      <p>Runtime: {movie.runtime} minutes</p>
+      <Link to="/">Go Back</Link>
+    </div>
   );
 }
 
