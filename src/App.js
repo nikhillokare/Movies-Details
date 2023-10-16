d7fe627f8b050e772291a6541a7e95f3bea6013e
Glitch (starter-cra-and-express) <none>
2023-10-16 08:25:13 +0000

    😘🎡 Checkpoint ./src/App.js:62151714/6614 ./src/SingleMovie.js:62151714/547 ./package.json:62151714/2116

--- src/App.js
+++ src/App.js
@@ -1,33 +1,63 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function SingleMovie({ movie }) {
+  if (!movie) {
+    return <div>No movie selected</div>;
+  }
+
+  return (
+    <div className="single-movie">
+      <h1>{movie.title}</h1>
+      <p>Tagline: {movie.tagline}</p>
+      <p>Vote Average: {movie.vote_average}/10</p>
+      <p>Release Date: {new Date(movie.release_date).toLocaleDateString()}</p>
+      <p>Runtime: {movie.runtime} minutes</p>
+      <Link to="/">Go Back</Link>
+    </div>
+  );
+}
 
 function App() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    async function getData() {
-      // Fetch movies data from the server
-      const response = await fetch('/api/movies');
-      const payload = await response.json();
-      setMovies(payload.data);
+    async function fetchData() {
+      try {
+        const response = await fetch('/api/movies');
+        if (response.ok) {
+          const data = await response.json();
+          setMovies(data.data);
+        }
+      } catch (error) {
+        console.error('Error fetching movie data:', error);
+      }
     }
-    getData();
+
+    fetchData();
   }, []);
 
   return (
-    <div className="App">
-      <h1>Movies List</h1>
-      <div className="movie-list">
-        {movies.map((movie) => (
-          <div className="movie-card" key={movie.id}>
-            <h2>{movie.title}</h2>
-            <p>Tagline: {movie.tagline}</p>
-            <p>Vote Average: {movie.vote_average}/10</p>
-          </div>
-        ))}
+    <Router>
+      <div className="App">
+        <h1>Movies List</h1>
+        <div className="movie-list">
+          {movies.map((movie) => (
+            <Link to={`/movie/${movie.id}`} key={movie.id}>
+              <div className="movie-card">
+                <h2>{movie.title}</h2>
+                <p>Tagline: {movie.tagline}</p>
+                <p>Vote Average: {movie.vote_average}/10</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+      <Routes>
+        <Route path="/movie/:id" element={<SingleMovie movie={movies[0]} />} />
+      </Routes>
+    </Router>
   );
 }
 
